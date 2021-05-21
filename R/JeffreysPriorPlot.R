cccc6dc909641d868df2f8abb0e37053ff7bfaa3
Bill Meeker <wqmeeker@iastate.edu>
2021-05-20 20:01:26 -0500

    Jeffreys prior stuff

--- R/JeffreysPriorPlot.R
+++ R/JeffreysPriorPlot.R
@@ -1,13 +1,13 @@
 #' Returns sqrt of the determinant of the FIM.
 #' @param distribution The probability distribution.
-#' @param std.log.censor.time Vector of standardized log censoring times. 
-#' @param unlist If TRUE and std.log.censor.time is a scalar, return a vector instead of a list.
-#' @return Silently returns  a results matrix containing the variance factors.
+#' @param prob.range range of probabilities for evaluation. 
+#' @return Silently returns  a results vectors.
 #' @examples
+#' JeffreysPriorPlot()
 #' @useDynLib lsinf
 #' @export
 ###
-"JeffreysPriorPlot" <- function(distribution="normal", prob.range= c(0.01, 0.99), number.points=100) {
+"JeffreysPriorPlot" <- function(distribution="normal", prob.range = c(0.01, 0.99), number.points=100, cex.axis=1.5, cex.lab=1.5) {
 ###
 ### this first cut works only for the normal distribution
 ###
@@ -34,13 +34,14 @@
         det[big.ones] <- lsinf.out$f11 * lsinf.out$f22 - lsinf.out$f12^2
     }
     result <- sqrt(det)
-  plot(range(std.log.censor.time), range(result), xaxt = "n", type = "n", xlab="Expected fraction failing", ylab="Sqrt Det scaled FIM" )
+  par(mar=c(5.1, 5.1, 4.1, 2.1))
+  plot(range(std.log.censor.time), range(result), xaxt = "n", type = "n", xlab="Expected fraction failing", ylab="Sqrt Det scaled FIM", las=1, cex.axis=cex.axis, cex.lab=cex.lab)
 ###
 ###  probability axis
 ###
   prob.labels=c("0.01", "0.05", "0.20", "0.50", "0.80", "0.95", "0.99")
   prob.locations= qnorm(as.numeric(prob.labels))
-  axis(side = 1, at = prob.locations, labels = prob.labels)
+  axis(side = 1, at = prob.locations, labels = prob.labels, cex.axis=cex.axis)
   lines(std.log.censor.time, result, type="l", lwd=2)
     invisible(list(result, std.log.censor.time))
 }
--- inst/Examples/SMRD2_verbatim13.R
+++ inst/Examples/SMRD2_verbatim13.R
@@ -6,13 +6,15 @@
 #-----------------------------------------------------------
 
 library(StatInt)
+library(lsinf)
 
 ### Example 13.9
 psev((log(50)-4.81)/0.50)
 scaledCovarianceMatrix("sev", -1.796)
 (1.96)^{2}*(6.281)/(log(2.0)^2)
 
-(1.96)^{2}*(scaledCovarianceMatrix("sev", (log(50)-4.81)/0.50)["v22"])/(log(2.0)^2)
+(1.96)^{2}*(scaledCovarianceMatrix("sev",
+             (log(50)-4.81)/0.50)["v22"])/(log(2.0)^2)
 
 
 
@@ -21,7 +23,8 @@ pnorm((log(120)-6.21)/1.4)
 scaledCovarianceMatrix("normal", (log(120)-6.21)/1.4)
 (1.96)^2*(4.685)/(log(2.0)^2)
 
-(1.96)^2*(scaledCovarianceMatrix("normal", (log(120)-6.21)/1.4)["v22"])/(log(2.0)^2)
+(1.96)^2*(scaledCovarianceMatrix("normal",
+        (log(120)-6.21)/1.4)["v22"])/(log(2.0)^2)
 
 
 
@@ -31,7 +34,8 @@ pweibull(50, shape=2, scale=exp(4.81))
 varianceFactorQuantile(0.10, psev(-1.796), "sev")
 (1.96)^2*(0.50)^2*(7.608)/(log(1.5)^2)
 
-(1.96)^2*(0.50)^2*(varianceFactorQuantile(0.10, psev((log(50)-4.81)/0.50), "sev"))/(log(1.5)^2)
+(1.96)^2*(0.50)^2*(varianceFactorQuantile(0.10,
+        psev((log(50)-4.81)/0.50), "sev"))/(log(1.5)^2)
 
 
 
@@ -40,4 +44,5 @@ pnorm((log(120)-6.21)/1.4)
 varianceFactorQuantile(0.10, 0.1548, "normal")
 (1.96)^2*(1.4)^2*(2.2)/(log(2.0)^2)
 
-(1.96)^2*(1.4)^2*(varianceFactorQuantile(0.10, pnorm((log(120)-6.21)/1.4), "normal"))/(log(2.0)^2)
+(1.96)^2*(1.4)^2*(varianceFactorQuantile(0.10,
+         pnorm((log(120)-6.21)/1.4), "normal"))/(log(2.0)^2)
