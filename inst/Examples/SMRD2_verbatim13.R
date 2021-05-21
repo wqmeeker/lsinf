cd9cd19ebf0e52df88e61588a8c7dd758698b628
Bill Meeker <wqmeeker@iastate.edu>
2021-05-21 18:30:16 -0500

    minor changes to the verbatim file

--- inst/Examples/SMRD2_verbatim13.R
+++ inst/Examples/SMRD2_verbatim13.R
@@ -8,7 +8,9 @@
 library(StatInt)
 library(lsinf)
 
+###
 ### Example 13.9
+###
 psev((log(50)-4.81)/0.50)
 scaledCovarianceMatrix("sev", -1.796)
 (1.96)^{2}*(6.281)/(log(2.0)^2)
@@ -17,8 +19,9 @@ scaledCovarianceMatrix("sev", -1.796)
              (log(50)-4.81)/0.50)["v22"])/(log(2.0)^2)
 
 
-
+###
 ### Example 13.10
+###
 pnorm((log(120)-6.21)/1.4)
 scaledCovarianceMatrix("normal", (log(120)-6.21)/1.4)
 (1.96)^2*(4.685)/(log(2.0)^2)
@@ -28,7 +31,9 @@ scaledCovarianceMatrix("normal", (log(120)-6.21)/1.4)
 
 
 
+###
 ### Example 13.11
+###
 (log(50)-4.81)/0.50 
 pweibull(50, shape=2, scale=exp(4.81))
 varianceFactorQuantile(0.10, psev(-1.796), "sev")
@@ -39,7 +44,9 @@ varianceFactorQuantile(0.10, psev(-1.796), "sev")
 
 
 
+###
 ### Example 13.12
+###
 pnorm((log(120)-6.21)/1.4)
 varianceFactorQuantile(0.10, 0.1548, "normal")
 (1.96)^2*(1.4)^2*(2.2)/(log(2.0)^2)
